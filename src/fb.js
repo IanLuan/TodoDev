cde7c682558ee876d4614c9851514b517bd733cf
Ian Luan <32806502+IanLuan@users.noreply.github.com>
2021-05-14 14:24:48 -0300

    Update fb.js

--- src/fb.js
+++ src/fb.js
@@ -4,13 +4,13 @@ require('firebase/auth')
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyA2VM3IU0FLjhcs-iDGa_iXd5rnJcmPdWU",
-  authDomain: "todoteam-3263d.firebaseapp.com",
-  databaseURL: "https://todoteam-3263d.firebaseio.com",
-  projectId: "todoteam-3263d",
-  storageBucket: "todoteam-3263d.appspot.com",
-  messagingSenderId: "643524690865",
-  appId: "1:643524690865:web:1b7e4938dad3e2e4"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
